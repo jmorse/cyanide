b1446dd69d3fae7a13b62c11d99a9a463e131044
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-01-17 17:56:29 +0000

    Dump poll response as json, not almost-json

--- bin/poll.py
+++ bin/poll.py
@@ -3,7 +3,7 @@
 import sys
 import pygit2
 import re
-import yaml
+import json
 from config import get_repo
 
 team_name = sys.argv[1]
@@ -14,4 +14,6 @@ refs = dict()
 
 for ref in repo.match_reference_glob('refs/heads/{0}/*'.format(team_name)):
     match = regexp.match(ref.name)
-    sys.stdout.write("{0}: {1}\n".format(match.group(1), ref.target.hex))
+    refs[match.group(1)] = ref.target.hex
+
+print json.dumps(refs)
